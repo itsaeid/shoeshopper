74509d66cf70ea4eac9742f271c09ce52acfdf59
itsaeed0 <ashourisaeid78@gmail.com>
2024-11-18 14:49:31 +0330

    ginish first page

---
+++ src/onloading/second.js
@@ -0,0 +1,20 @@
+import { El } from "../createElement";
+
+export function secondWelcome(){
+    const secWelcome = El({
+        const firstLoad = El({
+            element: "div",
+            className:
+              "bg-white h-[100vh] w-[428px] flex flex-col justify-center items-center gap-24",
+
+            children: [
+                El({
+                    element: "img",
+                    className: "bg-red",
+
+                })
+            ]
+    })
+}
+return secWelcome();
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'welcombanner': "url('src/img/welcomebanner.jpg')",
+        
+      }
+    },
   },
   plugins: [],
 }
